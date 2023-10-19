e734096606a979f0c8d5a273f259c8325877be9d
Seyi <seyi.k.akinwale@gmail.com>
2023-10-19 05:21:31 -0700

    api call

--- app/src/main/java/com/example/randompetlab/MainActivity.kt
+++ app/src/main/java/com/example/randompetlab/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.randompetlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.getPhotoButton)
+        button.setOnClickListener {
+            //make a network call to NASA API
+            getPetImage()
+        }
+    }
+
+    private fun getPetImage(){
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["limit"] = "5"
+        params["page"] = "0"
+
+        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Dog", "response successful")
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Dog Error", errorResponse)
+            }
+        }]
     }
 }
